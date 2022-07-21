8e6488981e02bc25231a3f88e186877dc8fa2499
nbelousow <niklas.belousow@ibykus.de>
2022-07-21 16:45:00 +0200

    get method for retrieving post by id

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ from typing import Optional
 from fastapi import Body, FastAPI
 import uvicorn
 from pydantic import BaseModel # schema validation
+from random import randrange
 
 app = FastAPI()
 
 # class for schema validation; inherits from pydantic
+# checks the values in body for the data types in class Post
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
     # does the same thing as just default values
     rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favourite foods", "content": "Pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 # app.get() als path operation decorator 
 # http request get als operation
@@ -24,21 +34,33 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is my new post."}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+# path parameter id in url wird extrahiert
+# VORSICHT!!! path parameter könnte mit einem anderen path matchen (z.B. /posts/latest)
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    print(id)
+    return {"post_detail": post}
+
+@app.post("/posts")
 #def create_posts(payload: dict = Body(...)):
     #print(payload)
     #return {"new_post": f"title {payload['title']} content {payload['content']}"}
 
-#parameter gets the pydantic model Post as validation schema
+# parameter gets the pydantic model Post as validation schema
+# In Post wird der request body validiert und abgespeichert
 def create_posts(new_post: Post):
     print(new_post)
     print(new_post.title)
     print(new_post.published)
     print(new_post.rating)
     print(new_post.dict())
-    return {"data": new_post}
+    post_dict = new_post.dict()
+    post_dict["id"] = randrange(0,100000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
     
 
 

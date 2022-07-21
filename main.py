64c64fde5de4fbd72fa9d23c77675162e6eb73d7
nbelousow <niklas.belousow@ibykus.de>
2022-07-21 13:47:01 +0200

    added post methods with body parameter

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 import uvicorn
 
 app = FastAPI()
 
+# app.get() als path operation decorator 
+# http request get als operation
+# "/" als path bzw. route
 
+# root() als path operation function
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello world!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is my new post."}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title {payload['title']} content {payload['content']}"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

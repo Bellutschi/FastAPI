aa7400d5db8a2340147c8a2440ccf61976ea2045
nbelousow <niklas.belousow@ibykus.de>
2022-07-21 16:08:43 +0200

    expanded the validatoin schema class Post

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from typing import Optional
 from fastapi import Body, FastAPI
 import uvicorn
+from pydantic import BaseModel # schema validation
 
 app = FastAPI()
+
+# class for schema validation; inherits from pydantic
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    # does the same thing as just default values
+    rating: Optional[int] = None
 
 # app.get() als path operation decorator 
 # http request get als operation
@@ -17,9 +27,19 @@ def get_posts():
     return {"data": "This is my new post."}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title {payload['title']} content {payload['content']}"}
+#def create_posts(payload: dict = Body(...)):
+    #print(payload)
+    #return {"new_post": f"title {payload['title']} content {payload['content']}"}
+
+#parameter gets the pydantic model Post as validation schema
+def create_posts(new_post: Post):
+    print(new_post)
+    print(new_post.title)
+    print(new_post.published)
+    print(new_post.rating)
+    print(new_post.dict())
+    return {"data": new_post}
+    
 
 
 if __name__ == "__main__":

f641308ec51014fe808b58788eefdb1cc01e1e76
nbelousow <niklas.belousow@ibykus.de>
2022-08-10 14:00:25 +0200

    added hashed password request

--- app/main_orm.py
+++ app/main_orm.py
@@ -1,18 +1,15 @@
 from typing import Optional, List
 from fastapi import Body, FastAPI, Response, status, HTTPException, Depends
 import uvicorn
-from pydantic import BaseModel # schema validation
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 import models, schemas
 from database import engine, get_db
 from sqlalchemy.orm import Session
+import utils
 
 # creates all tables stored in Base.metadata
 # if __table_name__ is available it doesnt create table
 models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
@@ -85,12 +82,27 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # hash the password
+    hashed_pwd = utils.hash(user.password)
+    user.password = hashed_pwd
+
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
     return new_user
+
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist")
+
+    return user
+
     
 
 
